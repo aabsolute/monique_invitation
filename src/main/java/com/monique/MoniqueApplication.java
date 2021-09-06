491cbda093b5f976ebe7642484ee7be07420510a
aabsolute <aabsolute87@yahoo.co.jp>
2021-09-07 00:10:08 +0900

    Initial commit

---
+++ src/main/java/com/monique/MoniqueApplication.java
@@ -0,0 +1,13 @@
+package com.monique;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoniqueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoniqueApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/monique/MoniqueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.monique;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoniqueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
