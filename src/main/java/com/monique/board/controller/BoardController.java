33ed4356b8657cf0050e0bbcbe25513ab78dc0b7
kwakseonghwan <aabsolute87@yahoo.co.jp>
2021-11-16 07:26:52 +0900

    board start

--- src/main/java/com/monique/board/controller/BoardController.java
+++ src/main/java/com/monique/board/controller/BoardController.java
@@ -1,11 +1,30 @@
 package com.monique.board.controller;
 
+import com.monique.board.service.BoardService;
+import com.monique.domain.Board;
+import com.monique.domain.Gallery;
+import com.monique.gallery.service.GalleryService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Slf4j
 @RequiredArgsConstructor
 @Controller
 public class BoardController {
+
+
+    private final BoardService boardService;
+
+    @GetMapping("blog")
+    public String getBlog(Model model, @RequestParam(value = "page", defaultValue = "0") int page){
+
+        Page<Board> galleryList = boardService.getAllBoardWithPaging(page);
+        model.addAttribute("paging", galleryList );
+        return "gallery/gallery";
+    }
 }
--- src/main/java/com/monique/board/service/BoardService.java
+++ src/main/java/com/monique/board/service/BoardService.java
@@ -2,9 +2,18 @@ package com.monique.board.service;
 
 
 import com.monique.board.repository.BoardRepository;
+import com.monique.common.enums.CommonCode;
+import com.monique.domain.Board;
+import com.monique.domain.Gallery;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -12,4 +21,14 @@ import org.springframework.stereotype.Service;
 public class BoardService {
 
     private final BoardRepository boardRepo;
+
+    @Autowired
+    ModelMapper modelMapper;
+
+    // 1. admin galleryList -> All gallery with paging order by createdDATE
+    @Transactional(readOnly = true)
+    public Page<Board> getAllBoardWithPaging(int pageNum)
+    {
+        return boardRepo.findAll(PageRequest.of(pageNum==0? 0:pageNum-1, CommonCode.PAGE_POST_COUNT, Sort.by(Sort.Direction.DESC, "createdDate")));
+    }
 }
